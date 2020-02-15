8dcfe81eb4e371a86af122d69dab0bd4a01ee867
ChristianArroyoAuz <christian.xavier.arroyo.auz@hotmail.com>
2020-02-14 19:38:51 -0500

    C#

---
+++ Sistema de Evacuacion/Sistema de Evacuacion/DB/MyDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+
+namespace Sistema_de_Evacuacion.DB
+{
+    class MyDB : DataContext
+    {
+        private const String cadenaConexion = "CADENA_DE_CONEXION";        
+        public Table<Tabla_Usuario> Usuarios;
+        public MyDB() : base(cadenaConexion) { }
+    }
+}
